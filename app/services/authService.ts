953e763f08a0173b4933f4399268d42baf994826
Ricardo <ricardomelida92@gmail.com>
2024-12-29 23:07:08 -0300

    has been added services example

---
+++ app/services/authService.ts
@@ -0,0 +1,9 @@
+import { ICredential } from "@/models/interfaces/ICredential"
+
+export const signInService = async (userCredential: ICredential) => {
+    return await 'something'
+}
+
+export const signOutService = async () => {
+    return await 'something'
+}
