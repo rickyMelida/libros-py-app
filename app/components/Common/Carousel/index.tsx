981a5c7150347a55c8943d72d5f01745fdc05c90
Ricardo Melida <40615609+rickyMelida@users.noreply.github.com>
2026-05-23 09:10:21 -0300

    has been update card styles

--- app/components/Common/Carousel/index.tsx
+++ app/components/Common/Carousel/index.tsx
@@ -8,10 +8,18 @@ import styles from "./index.module.css";
 const Index = ({ images }: { images: StaticImageData[] }) => {
        return (
 	       <div className={styles["carousel-responsive-container"]}>
-		       <Swiper
-			       spaceBetween={20}
-			       slidesPerView={2}
-		       >
+			   <Swiper
+				   spaceBetween={20}
+				   slidesPerView={3}
+				   breakpoints={{
+					   0: {
+						   slidesPerView: 2,
+					   },
+					   768: {
+						   slidesPerView: 3,
+					   },
+				   }}
+			   >
 			       {images.map((img, index) => (
 				       <SwiperSlide key={index}>
 					       <Image
