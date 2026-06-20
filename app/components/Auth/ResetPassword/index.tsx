d1aef303f6e64515cad3c6956d4a02facf3d10d2
Ricardo Melida <40615609+rickyMelida@users.noreply.github.com>
2026-06-20 16:10:35 -0300

    has been fix credencial model

--- app/components/Auth/ResetPassword/index.tsx
+++ app/components/Auth/ResetPassword/index.tsx
@@ -16,6 +16,7 @@ const Index = () => {
 	const supabase = createClient();
 
 	const [userData, setUserData] = useState<IUserCredential>({
+		email: "",
 		password: "",
 		confirmPassword: "",
 	});
